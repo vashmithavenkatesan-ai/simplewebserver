88b51e833c82a5b206ff22ae729bd750acb712d3
Vashmitha <vashmithavenkatesan@gmail.com>
2025-09-16 15:05:04 +0530

    Success

--- vashmitha.py
+++ vashmitha.py
@@ -1,42 +1,6 @@
-# EX01 Developing a Simple Webserver
-## Date:09.09.2025
-
-## AIM:
-To develop a simple webserver to serve html pages and display the Device Specifications of your Laptop.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Import the necessary modules.
-
-### Step 5:
-Define a custom request handler.
-
-### Step 6:
-Start an HTTP server on a specific port.
-
-### Step 7:
-Run the Python script to serve web pages.
-
-### Step 8:
-Serve the HTML pages.
-
-### Step 9:
-Start the server script and check for errors.
-
-### Step 10:
-Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
-
-## PROGRAM:
-```
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
 <html>
     <head>
         <tittle>
@@ -64,7 +28,7 @@ Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
             <tr>
                 <td>3</td>
                 <td>Installed RAM</td>
-                <td>16.0 GB (15.5 GB usable)</td>
+            ]     <td>16.0 GB (15.5 GB usable)</td>
             </tr>
             <tr>
                 <td>4</td>
@@ -81,10 +45,15 @@ Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
         </table>
     </body>
 </html>
-```
-
-## OUTPUT:
-![alt text](<Screenshot (21).png>)
-![alt text](<Screenshot (20).png>)
-## RESULT:
-The program for implementing simple webserver is executed successfully.
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',8000)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
